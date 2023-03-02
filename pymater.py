90dcad7f07293e8d6d39fb615b630d39d257ad0b
Keith Gyarmati <kvgyarmati@gmail.com>
2023-03-02 09:10:41 -0500

    03_02_23 - corrected block comment to docstring

--- pymater.py
+++ pymater.py
@@ -1,4 +1,4 @@
-/*
+"""
                     GNU GENERAL PUBLIC LICENSE
                        Version 3, 29 June 2007
 
@@ -674,8 +674,7 @@ the library.  If this is what you want to do, use the GNU Lesser General
 Public License instead of this License.  But first, please read
 <https://www.gnu.org/licenses/why-not-lgpl.html>.
 
-
-*/
+"""
 
 
 
